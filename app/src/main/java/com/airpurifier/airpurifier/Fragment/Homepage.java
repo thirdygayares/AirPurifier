fa423f5f4077a7ed882fc045272f592e10ce4386
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2023-09-20 13:33:14 +0800

    login

---
+++ app/src/main/java/com/airpurifier/airpurifier/Fragment/Homepage.java
@@ -0,0 +1,16 @@
+package com.airpurifier.airpurifier.Fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.airpurifier.airpurifier.R;
+
+public class Homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+}
